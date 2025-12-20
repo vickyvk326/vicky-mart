a94c19a93a9df58ee89f61e17dab551750b9d115
Vignesh Ravichandran <114295235+vickyvk326@users.noreply.github.com>
2025-12-21 00:28:13 +0530

    Improved logging

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,22 +1,52 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
+import * as path from 'path';
+import * as fs from 'fs';
+
+const logDir = path.join(process.cwd(), 'logs');
+if (!fs.existsSync(logDir)) fs.mkdirSync(logDir);
 
 @Module({
   imports: [
     PinoLoggerModule.forRoot({
-      pinoHttp:
-        process.env.NODE_ENV === 'development'
-          ? {
-              transport: {
-                target: 'pino-pretty',
-                options: {
-                  singleLine: true,
-                  colorize: true,
+      pinoHttp: {
+        level: 'debug',
+        transport: {
+          targets: [
+            {
+              target: 'pino-pretty',
+              options: {
+                colorize: true,
+                singleLine: false,
+                translateTime: 'SYS:dd/mm/yyyy, h:MM:ss TT',
+                ignore: 'hostname,req,res,context,pid',
+                messageFormat: '[{context}] - {time}: {msg}',
+                hideObject: true,
+              },
+            },
+            {
+              target: 'pino/file', // write logs to file
+              options: {
+                destination: path.join(logDir, `app.log`),
+                mkdir: true,
+              },
+            },
+            {
+              target: 'pino-roll',
+              options: {
+                file: path.join(logDir, 'app'), // Base filename
+                frequency: 'daily', // Roll every day
+                size: '10m', // Also roll if file exceeds 10MB
+                mkdir: true,
+                extension: '.log', // File will be app.2025-12-21.log
+                limit: {
+                  count: 30,
                 },
               },
-              level: 'debug',
-            }
-          : { level: 'info' },
+            },
+          ],
+        },
+      },
     }),
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PinoLogger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true, // buffer logs until logger is ready
+  });
+
+  const logger = await app.resolve(PinoLogger);
+  logger.setContext('Bootstrap');
+
   await app.listen(process.env.PORT ?? 3000);
+
+  logger.info(
+    `NestJS application bootstrapped successfully on port ${process.env.PORT || 3000}`,
+  );
 }
 bootstrap();
